27195a2b71781fc9f6c2061434eeeaa22e20e1c2
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-04-22 21:01:57 -0300

    tests

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -6,16 +6,142 @@ namespace Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+
+        [Test]
+        public void TestCedula1()
         {
-            // Insertá tu código de inicialización aquí
+            // Pruebo si cedula es null
+            string cedula = "";
+            bool actual = IdUtils.IdIsValid(cedula);
+            bool expected = false;
+            Assert.AreEqual(expected, actual);
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void TestCedula2()
         {
-            // Insertá tu código  de pruebaaquí
+            // Pruebo si cedula tiene más números de los que debería
+            string cedula = "522716195";
+            bool actual = IdUtils.IdIsValid(cedula);
+            bool expected = false;
+            Assert.AreEqual(expected, actual);
         }
+
+        [Test]
+        public void TestCedula3()
+        {
+            // Pruebo con cedula invalida
+            string cedula = "12345678";
+            bool actual = IdUtils.IdIsValid(cedula);
+            bool expected = false;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCedula4()
+        {
+            // Pruebo si cedula tiene menos numeros de los que deberia
+            string cedula = "5227161";
+            bool actual = IdUtils.IdIsValid(cedula);
+            bool expected = false;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCedula5()
+        {
+            // Pruebo con letras en lugar de números
+            string cedula = "abcdefgh";
+            bool actual = IdUtils.IdIsValid(cedula);
+            bool expected = false;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCedula6()
+        {
+            // Pruebo con una cédula correcta
+            string cedula = "52271619";
+            bool actual = IdUtils.IdIsValid(cedula);
+            bool expected = true;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCedula7()
+        {
+            // Pruebo los puntos y guiones
+            string cedula = "5.227.161-9";
+            bool actual = IdUtils.IdIsValid(cedula);
+            bool expected = true;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCedula8() // Cambiá el nombre para indicar qué estás probando
+        {
+            // Pruebo los puntos y guiones
+            string cedula = "5227.161-9";
+            bool actual = IdUtils.IdIsValid(cedula);
+            bool expected = true;
+            Assert.AreEqual(expected, actual);        }
+        
+        [Test]
+        public void TestCedula9() // Cambiá el nombre para indicar qué estás probando
+        {
+            // Pruebo los puntos y guiones
+            string cedula = "5227161-9";
+            bool actual = IdUtils.IdIsValid(cedula);
+            bool expected = true;
+            Assert.AreEqual(expected, actual);        }
+        
+        [Test]
+        public void TestCedula10() // Cambiá el nombre para indicar qué estás probando
+        {
+            // Pruebo los puntos y guiones
+            string cedula = "5.227161-9";
+            bool actual = IdUtils.IdIsValid(cedula);
+            bool expected = true;
+            Assert.AreEqual(expected, actual);        }
+        
+        [Test]
+        public void TestCedula11() // Cambiá el nombre para indicar qué estás probando
+        {
+            // Pruebo los puntos y guiones
+            string cedula = "5227161-9";
+            bool actual = IdUtils.IdIsValid(cedula);
+            bool expected = true;
+            Assert.AreEqual(expected, actual);        }
+        
+        [Test]
+        public void TestCedula12() // Cambiá el nombre para indicar qué estás probando
+        {
+            // Pruebo los puntos y guiones
+            string cedula = "5.227.1619";
+            bool actual = IdUtils.IdIsValid(cedula);
+            bool expected = true;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestNombre1()
+        {
+            // Pruebo si el nombre es null
+            Person pruebaNombre = new Person("", "52271619");
+            string actual = pruebaNombre.Name;
+            string expected = null;
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void TestNombre2()
+        {
+            // Pruebo si el nombre es valido
+            Person pruebaNombre = new Person("Ricardo", "52271619");
+            string actual = pruebaNombre.Name;
+            string expected = "Ricardo";
+            Assert.AreEqual(expected, actual);
+        }
+        
     }
 }
